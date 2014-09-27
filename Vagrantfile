df4eae4f0f6b342ef95a7dd5c08eff9d743f84bf
camael24 <thehawk@hoa-project.net>
2014-09-27 20:54:52 +0200

    First init

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  
+  config.vm.box = "ubuntu13"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.synced_folder "./www", "/vagrant_data"
+  config.omnibus.chef_version = :latest
+  config.vm.provision "chef_solo" do |chef|
+    chef.cookbooks_path = ["cookbooks", "site-cookbooks"]
+    chef.roles_path = "roles"
+    chef.data_bags_path = "data_bags"
+    chef.add_role = "webserver"
+  
+    # You may also specify custom JSON attributes:
+    # chef.json = { }
+  end
+
+end
---
+++ site-cookbooks/nginx-fpm/recipes/default.rb
@@ -0,0 +1,43 @@
+require_recipe "apt"
+require_recipe "php"
+require_recipe "memcached"
+require_recipe "imagemagick"
+require_recipe "hosts"
+
+package "php5-fpm" do
+  action :install
+end
+package "php5-curl" do
+  action :install
+end
+
+package "php5-gd" do
+  action :install
+end
+
+package "php5-mysql" do
+  action :install
+end
+
+package "php5-memcache" do
+  action :install
+end
+
+package "php5-sqlite" do
+  action :install
+end
+
+package "libpcre3-dev" do
+  action :install
+end
+
+template "#{node['nginx']['dir']}/sites-available/default" do
+  source 'sites.conf.erb'
+  owner  node['nginx']['user']
+  group  node['nginx']['group']
+  mode   '0644'
+  variables({
+    :x_men => node['foo']['foobar']
+  })
+  notifies :reload, 'service[nginx]'
+end
