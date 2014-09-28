70afddd784771e30ef961c2f57c514afcdeea8d2
camael24 <thehawk@hoa-project.net>
2014-09-28 16:02:06 +0200

    add capistrano

--- Vagrantfile
+++ Vagrantfile
@@ -6,6 +6,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   
   config.vm.box = "ubuntu13"
+  # config.vm.box = "ubuntu/trusty32"
   config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.synced_folder "./www", "/vagrant_data"
   config.omnibus.chef_version = :latest
@@ -13,7 +14,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     chef.cookbooks_path = ["cookbooks", "site-cookbooks"]
     chef.roles_path = "roles"
     chef.data_bags_path = "data_bags"
-    chef.add_role "webserver"
+    chef.add_role ("webserver")
+    chef.add_role ("zsh")
+    chef.add_role ("php-dev")
   
     # You may also specify custom JSON attributes:
     chef.json = {
--- site-cookbooks/nginx-fpm/recipes/default.rb
+++ site-cookbooks/nginx-fpm/recipes/default.rb
@@ -4,36 +4,12 @@ require_recipe "memcached"
 require_recipe "imagemagick"
 require_recipe "hosts"
 
-package "php5-fpm" do
-  action :install
+%w{ aptitude git curl vim htop bzip2 gcc make }.each do |a_package|
+  package a_package
 end
 
-package "php5-curl" do
-  action :install
-end
-
-package "git" do
-  action :install
-end
-
-package "php5-gd" do
-  action :install
-end
-
-package "php5-mysql" do
-  action :install
-end
-
-package "php5-memcache" do
-  action :install
-end
-
-package "php5-sqlite" do
-  action :install
-end
-
-package "libpcre3-dev" do
-  action :install
+%w{ php5-fpm php5-curl php5-gd php5-mysql php5-memcache php5-sqlite libpcre3-dev }.each do |a_package|
+  package a_package
 end
 
 template "#{node['nginx']['dir']}/sites-available/default" do
