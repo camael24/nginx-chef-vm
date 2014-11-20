587e1d330251858cd38cf7dad16299d03077286e
thehawk <thehawk@hoa-project.net>
2014-11-20 09:36:15 +0100

    No capistrano there, we deploy only website

--- config/deploy.rb
+++
@@ -1,8 +0,0 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
-
-set :application, 'foo'
-set :scm, :git
-set :server_name, "localhost"
-set :keep_releases, 5
-set :repo_url, 'git@bitbucket.org:arkvenger/nox-admin.git'
